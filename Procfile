c75d47a707450af10ad23c223391f65df499e3ee
nchah <nchah@users.noreply.github.com>
2021-04-18 14:27:36 -0400

    Updated into one app page

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bokeh serve --port=$PORT --address=0.0.0.0 --allow-websocket-origin=wd-bokeh.herokuapp.com --use-xheaders bokeh-tsne-wikidata-lang-locales.py
+web: bokeh serve --port=$PORT --address=0.0.0.0 --allow-websocket-origin=wd-bokeh.herokuapp.com --use-xheaders bokeh-tsne-wikidata-lang-locales-and-P31_obj_sets.py
--- bokeh-tsne-wikidata-lang-locales-and-P31_obj_sets.py
+++ bokeh-tsne-wikidata-lang-locales-and-P31_obj_sets.py
@@ -7,32 +7,9 @@ from bokeh.layouts import row, column
 from bokeh.themes import built_in_themes, Theme
 
 
-# def handler(event):
-#     global runtime, source1, show_labels
-#     # Load from source, then update data source on additional selections
-#     print('source1 data: ' + event.item)
-#     if runtime == 0:
-#         source1_data = pickle.load(open('source1-data/' + event.item,'rb'))
-#         source1_df = pd.DataFrame(source1_data, columns=source1_data.keys())
-#         source1 = ColumnDataSource(source1_df)  # can also load directly from dict instead of pandas
-#     elif runtime > 0:
-#         source1.data = pickle.load(open('source1-data/' + event.item,'rb'))
-
-#     # Scatter plot
-#     p.scatter(source=source1, x="x", y="y", color="colors", fill_alpha=0.7, size=5)
-
-#     # Add node labels
-#     print("check: " + str(show_labels))
-#     if show_labels == True:
-#         labels = LabelSet(x='x', y='y', text='lang', text_font_size='12px',
-#                 x_offset=5, y_offset=5, source=source1,
-#                 render_mode='canvas') #render_mode='canvas')
-#         p.add_layout(labels)
-#     elif show_labels == False:
-#         pass
-
-#     runtime += 1
-
+# # # # # # # # # # # # # # 
+# # Lang locales
+# # # # # # # # # # # # # # 
 
 def button_handler(event):
     global runtime, source1, show_labels
@@ -46,6 +23,7 @@ def button_handler(event):
         source1 = ColumnDataSource(source1_df)  # can also load directly from dict instead of pandas
     elif runtime > 0:
         source1.data = pickle.load(open('source1-data-langs/' + sourcefile,'rb'))
+    # print(source1_df)
 
     # Scatter plot
     p.scatter(source=source1, x="x", y="y", color="colors", fill_alpha=0.7, size=5)
@@ -79,7 +57,6 @@ def checkbox_handler(event):
         c.disabled = True
 
 
-
 ## Globals ##
 TOOLS="wheel_zoom,hover,crosshair,pan,zoom_in,zoom_out,box_zoom,undo,redo,reset,save"
 # TOOLS="wheel_zoom,zoom_in,zoom_out,reset,save"
@@ -104,8 +81,9 @@ p = figure(title = "Clusters of language locales by P31_obj_set localization fre
 
 menu_items = [' ',
     ' = = = = = = = = = = Top silhouette score:',
+    'source1-tSNE-Langs-Perplexity_25-LearningRate_200-Iterations_12500-KmeansClusters_5-AvgSilhouette_0.7744107.pickle',
     'source1-tSNE-Langs-Perplexity_30-LearningRate_250-Iterations_7500-KmeansClusters_5-AvgSilhouette_0.7843169.pickle',
-    ' = = = = = = = = = = ',
+    ' = = = = = = = = = = '
     'source1-tSNE-Langs-Perplexity_5-LearningRate_100-Iterations_10000-KmeansClusters_5-AvgSilhouette_0.5356258.pickle',
     'source1-tSNE-Langs-Perplexity_5-LearningRate_100-Iterations_5000-KmeansClusters_5-AvgSilhouette_0.4785947.pickle',
     'source1-tSNE-Langs-Perplexity_5-LearningRate_100-Iterations_7000-KmeansClusters_5-AvgSilhouette_0.49775645.pickle',
@@ -170,7 +148,7 @@ div = Div(text="""
 row1 = column(div,c,s,b)
 
 # para = Paragraph(text="""GitHub source: """, width=200, height=500)
-para = Div(text="<br><br><br><br><br> GitHub source:", 
+para = Div(text="<br><br> GitHub source: TBA <br><br><br><br><br><br><br>", 
           width=500, height=500, style={})
 
 theme = {
@@ -180,6 +158,132 @@ theme = {
         'text_font_size': '18px',
     }
 }}
+
+
+
+
+
+
+
+
+
+# # # # # # # # # # # # # # 
+# # P31_obj_set
+# # # # # # # # # # # # # # 
+
+def button_handler2(event2):
+    global runtime2, source2, show_labels2
+
+    # Load from source, then update data source on additional selections
+    sourcefile = "source1-tSNE-P31objset-" + s2.value.replace("NLP model: stsb-roberta-large", "NLP, stsb, roberta, large")\
+                                                     .replace(": ", "_")\
+                                                     .replace(", ", "-") + ".pickle"
+    print('source2 data: ' + s2.value)
+    if runtime2 == 0:
+        source2_data = pickle.load(open('source1-data-P31objset/' + sourcefile,'rb'))
+        source2_df = pd.DataFrame(source2_data, columns=source2_data.keys())
+        source2 = ColumnDataSource(source2_df)  # can also load directly from dict instead of pandas
+    elif runtime2 > 0:
+        source2.data = pickle.load(open('source1-data-P31objset/' + sourcefile,'rb'))
+
+    # Scatter plot
+    p2.scatter(source=source2, x="x", y="y", color="colors", fill_alpha=0.7, size=5)
+    p2.add_tools(HoverTool(tooltips=[("P31_obj_set (en)", "@P31_obj_set_id_label")]))
+
+    # Add node labels
+    print("show_labels: " + str(show_labels2))
+    if show_labels2 == True:
+        labels = LabelSet(x='x', y='y', text='P31_obj_set_id_label', text_font='helvetica', text_font_size='12px',
+                x_offset=5, y_offset=5, source=source2,
+                render_mode='canvas') #render_mode='canvas' or 'css')
+        p2.add_layout(labels)
+    elif show_labels2 == False:
+        pass
+
+    runtime2 += 1
+
+
+def checkbox_handler2(event):
+    global show_labels2, runtime2
+    # print(attr, old, new)
+    print(c2.active)
+    if 0 in c2.active:
+        show_labels2 = True
+        c2.labels = [' Node labels ON']
+    if 0 not in c2.active:
+        show_labels2 = False
+        c2.labels = [' Node labels OFF']
+    if runtime2 > 0:
+        c2.labels = [' (Please reload page to toggle again)']
+        c2.disabled = True
+
+
+
+## Globals ##
+TOOLS="wheel_zoom,hover,crosshair,pan,zoom_in,zoom_out,box_zoom,undo,redo,reset,save"
+# TOOLS="wheel_zoom,zoom_in,zoom_out,reset,save"
+runtime2 = 0
+source2 = ColumnDataSource()
+show_labels2 = True
+
+
+# Figure and Dropdown menu
+p2 = figure(title = "Clusters of P31_obj_set by their localization frequency across languages",
+           background_fill_color='whitesmoke',  # gainsboro
+           # tools=TOOLS, 
+           toolbar_location='below',
+           aspect_ratio=2,
+           # plot_height=1000,
+           # plot_width=2000, 
+           # width_policy='max',
+           # max_width=5000,
+           sizing_mode='stretch_both',
+           # tooltips=[("", "")],
+           active_scroll='wheel_zoom')
+
+menu_items2 = [' ',
+    ' = = = = = = = = = = Top silhouette score:',
+    ' = = = = = = = = = = = = 1 Hot Encoding: ',
+    'source1-tSNE-P31objset-1Hot-Perplexity_2-LearningRate_150-Iterations_15000-KmeansClusters_65-AvgSilhouette_0.86559206.pickle',
+    ' = = = = = = = = = = = = BERT Transformer Encoding: ',
+    'source1-tSNE-P31objset-NLP-stsb-roberta-large-Perplexity_2-LearningRate_200-Iterations_7500-KmeansClusters_325-AvgSilhouette_0.7104604.pickle',
+    '',
+    ' = = = = = = = = = = = = Misc. runs: ',
+    'source1-tSNE-P31objset-NLP-Perplexity_2-LearningRate_200-Iterations_15000-KmeansClusters_90-AvgSilhouette_0.5741877.pickle',
+    'source1-tSNE-P31objset-NLP-Perplexity_5-LearningRate_100-Iterations_5000-KmeansClusters_50-AvgSilhouette_0.500595.pickle',
+    'source1-tSNE-P31objset-NLP-Perplexity_2-LearningRate_100-Iterations_12500-KmeansClusters_65-AvgSilhouette_0.5196672.pickle'
+]
+# Formatting text for readability in dropdown
+menu_items2 = [ m.replace("source1-tSNE-P31objset-", "")
+                .replace("-", ", ").replace("_", ": ")
+                .replace(".pickle", "")
+                .replace("NLP, stsb, roberta, large", "NLP model: stsb-roberta-large") for m in menu_items2]
+
+c2 = CheckboxGroup(labels=["Show Node Labels"], active=[0])
+c2.on_click(checkbox_handler2)
+
+s2 = Select(title='Select dataset:', width=500, options=menu_items2)
+
+b2 = Button(label="Load Visualization", button_type="primary")
+b2.on_click(button_handler2)
+
+
+div2 = Div(text="""
+          Select the following settings and click "Load Visualization":<br>(1) Node labels on or off, <br>(2) Dataset source
+          <br> <hr> """, 
+          width=500, height=100, style={'font-size': '14px'})
+
+# row1 = row(div,c,s,b, align='center', cols='min')
+row2 = column(div2,c2,s2,b2)
+
+# para = Paragraph(text="""GitHub source: """, width=200, height=500)
+para2 = Div(text="<br><br> GitHub source: TBA <br><br><br><br><br><br><br>", 
+          width=500, height=500, style={})
+
+
+
+
+
 
 curdoc().title = "Bokeh Application"
 curdoc().theme = Theme(json=theme) #"dark_minimal"
@@ -191,6 +295,8 @@ curdoc().add_root(p)
 curdoc().add_root(row1)
 curdoc().add_root(para)
 
-
+curdoc().add_root(p2)
+curdoc().add_root(row2)
+curdoc().add_root(para2)
 
 
